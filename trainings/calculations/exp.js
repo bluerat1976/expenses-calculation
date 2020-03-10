174b5860ce5eb03993111a65db540c0bc7dd5737
bluerat1976 <osoblivaia76@mail.ru>
2020-03-10 20:11:02 +0200

    in process

--- trainings/calculations/exp.js
+++ trainings/calculations/exp.js
@@ -2,7 +2,9 @@
 let but = document.getElementById('btn');
 but.addEventListener('click', funk)
 but.addEventListener('click', funk2)
-
+let boxTotal = document.createElement('div')
+                    boxTotal.setAttribute('id', 'totalCurrent')
+                    document.body.appendChild(boxTotal)
        
         function funk() {
 
@@ -14,10 +16,15 @@ but.addEventListener('click', funk2)
        } 
    
         function funk2() {
+            let sum = 0;
+            let a;
+            
+
             let box = document.createElement('div');
             box.setAttribute('class', 'box2');
             document.body.appendChild(box)
-            
+            let p = document.createElement('p')
+
             btnCreate()
             
                 function btnCreate() {
@@ -29,11 +36,59 @@ but.addEventListener('click', funk2)
 
                     function addAmmount() {
                         let res = document.createElement('p')
-                        a = prompt('Input ammount')
+                        a = parseInt(prompt('Input ammount'))
                         res.innerHTML = a
                         box.appendChild(res)
                     }
+
+                function btnSumCreate() {
+                    let btnSum = document.createElement('button')
+                    btnSum.setAttribute('type', 'submit')
+                    btnSum.innerHTML = 'Summa'
+                    box.appendChild(btnSum)
+                    btnSum.addEventListener('click', getSum)
+                    
+                    function getSum() {
+                        
+                        p.setAttribute('style', 'backgroundColor')
+                        p.style.backgroundColor = '#cccccc'
+                        p.setAttribute('class', 'summ')
+                       sum += a 
+                        p.innerHTML = sum;
+                    // console.log(sum.toFixed(2))
+                        box.appendChild(p)
+                    }
+                }    
+                
+                btnSumCreate()
+
+                function btnTotal() {
+                    let btnT = document.createElement('button')
+                    btnT.setAttribute('type', 'submit')
+                    btnT.innerHTML = 'Sent to total'
+                    box.appendChild(btnT)
+                    btnT.addEventListener('click', totalSum)
+                    
+                    function totalSum() {
+                    
+                    let total = document.getElementsByClassName('summ')
+                    for(let i = 0; i<= total.length; i++) {
+                      total[i]++
+                      
+                    }
+
+                    document.getElementById('totalCurrent').innerHTML = total
+
                 }
+                totalSum()
+
+                }
+                btnTotal()
+
+            
+
+                totalCurrent
+            }
        }
        
         
